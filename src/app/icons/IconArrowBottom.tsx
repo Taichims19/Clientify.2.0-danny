c8d5d199e937e619d7deb73bc3016dde097e8bf6
Daniel Mendez <danielms694@gmail.com>
2024-11-07 22:46:33 -0400

    Avances en cajon datagrid , ajustes en proceso 1

---
+++ src/app/icons/IconArrowBottom.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+const IconArrowBottom = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={17}
+    fill="none"
+    {...props}
+  >
+    <g clipPath="url(#a)">
+      <path
+        fill="#6F6F6F"
+        d="M5.74 7.167a.444.444 0 0 0-.315.758l2.26 2.26a.445.445 0 0 0 .63 0l2.26-2.26a.444.444 0 0 0-.315-.758H5.74Z"
+      />
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path
+          fill="#fff"
+          d="M0 4.5a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4v8a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4v-8Z"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+);
+export default IconArrowBottom;
---
+++ src/app/icons/IconSearchFacture.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+const IconSearchFacture = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={17}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#8D8D8D"
+      fillRule="evenodd"
+      d="M3.895 7.237a2.842 2.842 0 1 1 5.684 0 2.842 2.842 0 0 1-5.684 0ZM6.737 2.5a4.737 4.737 0 1 0 2.612 8.689l3.034 3.034a.948.948 0 0 0 1.34-1.34l-3.034-3.034A4.737 4.737 0 0 0 6.737 2.5Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default IconSearchFacture;
